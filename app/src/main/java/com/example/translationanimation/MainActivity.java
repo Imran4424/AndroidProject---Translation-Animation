230f598078018376dee6f2eb84ce44d275ccd498
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-03-20 15:32:07 +0600

    setting UI

--- app/src/main/java/com/example/translationanimation/MainActivity.java
+++ app/src/main/java/com/example/translationanimation/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.translationanimation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void imagePressed(View v) {
+        
+    }
 }
